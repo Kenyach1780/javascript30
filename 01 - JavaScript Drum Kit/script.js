ca38325ed0d842ffe02bee8b0405e15255f71e8e
Kenya Harvey <KenyaCH1780@users.noreply.github.com>
2018-10-17 01:08:19 -0500

    finish drum kit

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!audio) return; // stops the function from running altogether if key with non-corresponding label is pressed
+  audio.currentTime = 0; // rewind to start of the sound
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; // skip if not a transform property
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
