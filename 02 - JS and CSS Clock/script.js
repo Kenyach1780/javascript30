f75e57c016c682d97e8355c057c48372af57e82c
Kenya Harvey <KenyaCH1780@users.noreply.github.com>
2018-10-24 15:09:17 -0500

    clock completed

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,11 +1,26 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
+
+  if (secondsDegrees === 90) {
+    secondHand.style.transition = 'none';
+  } else {
+    secondHand.style.transition = 'transform 0.2s cubic-bezier(0, 2.48, 0.72, 0.66)';
+  }
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
